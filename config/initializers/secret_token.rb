37a31916aac0181018340c99d7a3029169050fae
Sergio A. Morales <smorales@inf.utfsm.cl>
2013-02-19 18:15:08 -0300

    Proyecto Rails limpio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtg::Application.config.secret_token = '9d60bda6612c56731dc1162fd4b305e661e671e77f9cef17887fde354825e9932ee7a4fe7da9b3f0b29136e0d5dea2fd089c2f3713f53fa4bf19fd6faf294268'
